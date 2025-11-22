ed23506c31dd6c02a0c99ad5085358ec56066d69
Sushant Singh <sushantsingh@Sushants-MacBook-Air.local>
2025-11-22 16:11:04 +0530

    admin live url added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
         {/* ✅ Admin Panel Button - show only on home page  */}
         {location.pathname === '/' && (
           <button
-            onClick={() => window.open('https://doctor-management-app-admin.onrender.com', '_blank')}
+            onClick={() => window.open('https://doctor-management-app-admin.vercel.app', '_blank')}
             className='bg-primary text-white text-xs px-4 py-2 rounded-full hover:bg-gray-700 hidden md:block'
           >
             Admin Panel
